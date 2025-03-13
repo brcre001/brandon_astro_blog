f5e5e736f592754a44f11ce8c8293f263bdb2b20
Brandon Creed <brandon.f.creed@gmail.com>
2025-03-13 13:00:18 -0400

    added privacy and terms pages and improved some styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
             config: {
                 forward: ["dataLayer.push"],
             }
-        })
+        }),
+        tailwind()
     ]
 });
